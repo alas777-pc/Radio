8801b76818538bac5175f95559c000e13597d49f
Alexei Lubinets <lubinets01@mail.ru>
2024-08-28 23:20:29 +0500

    Added lambok

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,5 +1,12 @@
 package ru.netology;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Radio {
     private int currentStation;
     private int currentVolume;
@@ -72,3 +79,4 @@ public class Radio {
         }
     }
 }
+
