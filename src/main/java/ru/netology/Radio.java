091ee359b2bcd64ed48672c16b969b6543c0d81f
Alexei Lubinets <lubinets01@mail.ru>
2024-09-05 17:49:31 +0500

    Code and tests change

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -16,7 +16,69 @@ public class Radio {
     private int minVolume;
     private int maxVolume = 100;
 
+    public Radio(int countStation, int currentVolume) {
+        this.counterStation = countStation;
+        this.maxStation = countStation - 1;
+        this.currentVolume = currentVolume;
+    }
 
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > maxStation) {
+            return;
+        }
+        if (currentStation < minStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void stationNext() {
+        if (currentStation == maxStation) {
+            this.currentStation = minStation;
+            return;
+        }
+        currentStation = currentStation + 1;
+    }
+
+    public void stationPrev() {
+        if (currentStation == minStation) {
+            this.currentStation = maxStation;
+            return;
+        }
+        currentStation = currentStation - 1;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < maxVolume) {
+            currentVolume = currentVolume + 1;
+        } else {
+            this.currentVolume = maxVolume;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > minVolume) {
+            currentVolume = currentVolume - 1;
+        } else {
+            this.currentVolume = minVolume;
+        }
+    }
 }
 
 
